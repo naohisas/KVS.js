d1633f0e455619854f68acfccd560300b12280c8
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2014-06-18 23:56:29 +0900

    Modified.

--- Source/KVS2THREE.js
+++ Source/KVS2THREE.js
@@ -85,14 +85,10 @@ KVS.THREEScreen.prototype =
 
 KVS.ToTHREEParticleSystem = function( point )
 {
-    var geometry = new THREE.Geometry();
-    var material = new THREE.ParticleSystemMaterial();
-
-    material.size = point.size;
-
-    // Overwrite the default fragment shader defined in THREE.ParticleSystemMaterial()
-    // in order to draw a point as round-shaped.
-    material.fragmentShader =
+    // In order to draw a point as round-shaped point, the defulat fragment shader
+    // defined for THREE.ParticleSystemMaterial is over-written with the following
+    // fragment shader.
+    THREE.ShaderLib['particle_basic'].fragmentShader =
         [
             "uniform vec3 psColor;",
             "uniform float opacity;",
@@ -102,10 +98,10 @@ KVS.ToTHREEParticleSystem = function( point )
             THREE.ShaderChunk[ "shadowmap_pars_fragment" ],
             THREE.ShaderChunk[ "logdepthbuf_pars_fragment" ],
             "void main() {",
-            "    vec3 n;",
-            "    n.xy = gl_PointCoord * 2.0 - 1.0;",
-            "    n.z = 1.0 - dot( n.xy, n.xy );",
-            "    if ( n.z < 0.0 ) discard;",
+            /*add*/"vec3 n;",
+            /*add*/"n.xy = gl_PointCoord * 2.0 - 1.0;",
+            /*add*/"n.z = 1.0 - dot( n.xy, n.xy );",
+            /*add*/"if ( n.z < 0.0 ) discard;",
             "    gl_FragColor = vec4( psColor, opacity );",
             THREE.ShaderChunk[ "logdepthbuf_fragment" ],
             THREE.ShaderChunk[ "map_particle_fragment" ],
@@ -114,7 +110,12 @@ KVS.ToTHREEParticleSystem = function( point )
             THREE.ShaderChunk[ "shadowmap_fragment" ],
             THREE.ShaderChunk[ "fog_fragment" ],
             "}"
-        ];
+        ].join('\n');
+
+    var geometry = new THREE.Geometry();
+    var material = new THREE.ParticleSystemMaterial();
+
+    material.size = point.size;
 
     if ( point.colors.length == 0 )
     {
@@ -127,7 +128,7 @@ KVS.ToTHREEParticleSystem = function( point )
     }
     else // if ( point.colors.length == point.coords.length )
     {
-        material.vertexColors = THREE.VertexColors;
+        material.vertexColors = true;
         for ( var i = 0; i < point.colors.length; i++ )
         {
             var c = point.colors[i];
